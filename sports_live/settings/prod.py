760eb811ff15d36bf55e5c0a4042885e9e75b6a9
leehjhjhj <tsukiakarii@naver.com>
2023-10-04 21:04:58 +0900

    chore: first setting

---
+++ sports_live/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+from decouple import config
+import pymysql
+
+pymysql.install_as_MySQLdb()
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
---
+++ sports_live/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sports_live project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sports_live.settings.local')
+
+application = get_wsgi_application()
