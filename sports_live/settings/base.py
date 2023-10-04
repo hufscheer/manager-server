c91f5362fe5ade8c2587b8005c70a92fc3fe01ed
leehjhjhj <tsukiakarii@naver.com>
2023-10-04 21:58:57 +0900

    feat: 의존성 추가 및 docker compose 작성

--- sports_live/settings/base.py
+++ sports_live/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'data/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python manage.py collectstatic --no-input
+gunicorn sports_live.wsgi:application --bind 0.0.0.0:8000
