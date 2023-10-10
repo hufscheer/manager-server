dffdc7cea8fc038b64a46f645736c13da857dde8
leehjhjhj <tsukiakarii@naver.com>
2023-10-10 09:27:13 +0900

    refactor/게임수정정get변경

--- manage/services/game_score_change_service.py
+++ manage/services/game_score_change_service.py
@@ -1,10 +1,22 @@
 from manage.models.game import Game
 from manage.serializers.change_game_score_serializer import GameInfoGetSerializer, GameScoreChangePostSerializer
+from manage.serializers.game_team_list_serializer import GameListSerializer
 
 def get_game_info(game_id):
-    game = Game.objects.get(id=game_id)
+    game = Game.objects.select_related('first_team', 'second_team').get(id=game_id)
+    teams_data = {
+        'first_team': game.first_team,
+        'second_team': game.second_team,
+    }
+    team_list_serializer = GameListSerializer(teams_data)
+    team_list_data = team_list_serializer.data
+
     game_info_get_serialzier = GameInfoGetSerializer(game)
-    return game_info_get_serialzier.data
+    game_info_data = game_info_get_serialzier.data
+    game_info_data['teamList'] = team_list_data
+
+    return game_info_data
+
 
 def change_game_score(post_change_game_data, game_id):
     post_change_game_data['game'] = game_id
