eac04f90c8f77a2991306920214bb7c9c22596c9
leehjhjhj <tsukiakarii@naver.com>
2023-10-16 14:24:11 +0900

    HOTFIX: pull_images.sh 변경

--- scripts/pull_images_from_ecr.sh
+++ scripts/pull_images_from_ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PROJECT_PATH=/home/ec2-user/
 cd ${PROJECT_PATH}
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 279381197488.dkr.ecr.ap-northeast-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 550581268183.dkr.ecr.ap-northeast-2.amazonaws.com
 docker-compose pull
