709e21c982224364389a16e5f6bfbdfea83fdf7d
leehjhjhj <tsukiakarii@naver.com>
2023-11-03 14:10:25 +0900

    close #19 feat: 관리자 체크 permission 수정

--- accounts/models/is_admin_user.py
+++ accounts/models/is_admin_user.py
@@ -1,6 +1,8 @@
 from rest_framework.permissions import BasePermission
 
 class IsAdminUser(BasePermission):
+    message = "관리자 권한이 필요합니다."
+    
     def has_permission(self, request, view):
         is_administrator = bool(int.from_bytes(request.user.is_administrator, byteorder='big'))
         return bool(request.user and is_administrator)
