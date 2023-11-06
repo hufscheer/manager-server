32cecde135df08ec12037dc3c785c2f01cf365f6
leehjhjhj <tsukiakarii@naver.com>
2023-11-06 18:22:36 +0900

    fix: urls.py 오타 수정

--- sports_live/urls.py
+++ sports_live/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
-    path('gane/', include("gane.urls")),
+    path('gane/', include("game.urls")),
     path('league/', include("league.urls")),
     path('report/', include("report.urls")),
     path('team/', include("team.urls")),
