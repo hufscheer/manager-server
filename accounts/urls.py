7d9e9c1b8c12e140ea68cf05633af5df7ddec31c
leehjhjhj <tsukiakarii@naver.com>
2023-11-08 08:20:01 +0900

    feat: 비밀번호 암호화 api 추가 #28

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from .views.login_views import CustomTokenObtainPairView
-from .views.permission_test_view import PermissionTestView
+from .views.permission_test_view import PermissionTestView, MakePasswordTest
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('permission/', PermissionTestView.as_view(), name='PermissionTestView'),
+    path('makepassword/', MakePasswordTest.as_view(), name='MakePasswordTest'),
 ]
