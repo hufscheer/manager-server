0a64c1cdb9fe549b065088f09eab3c3277bb798c
leehjhjhj <tsukiakarii@naver.com>
2023-11-10 11:40:23 +0900

    fix: object 이름 분할 저장 #22

--- utils/upload_to_s3.py
+++ utils/upload_to_s3.py
@@ -1,16 +1,14 @@
 from decouple import config
 import boto3
 from rest_framework import exceptions
-import uuid
 
-def upload_to_s3(image_data, team_name: dict, league_name: str):
+def upload_to_s3(image_data, team_name: str, league_name: str):
     aws_access_key = config('S3_ACCESS_KEY')
     aws_secret_key = config('S3_SECRET_ACCESS_KEY')
     bucket_name = config('BUCKET_NAME')
-    team_name = team_name.get('name')
     try:
         s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
-        object_name = f"{team_name}.{image_data.content_type}"
+        object_name = f"{team_name}.{image_data.content_type.split('/')[-1]}"
         key = league_name + '/' + object_name
         s3.put_object(Body=image_data, Bucket=bucket_name, Key=key, ContentType='jpg')
         url = f"https://{bucket_name}.s3.ap-northeast-2.amazonaws.com/{key}"
