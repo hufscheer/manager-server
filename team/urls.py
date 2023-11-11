2391461e808759529cf0af44fe8b16a9445ff5b8
leehjhjhj <tsukiakarii@naver.com>
2023-11-11 13:25:16 +0900

    refactor: team url 수정 #22

--- team/urls.py
+++ team/urls.py
@@ -5,5 +5,6 @@ app_name = 'team'
 
 urlpatterns = [
     path('register/<int:league_id>/', TeamView.as_view()),
-    path('<int:league_id>/', TeamGetView.as_view())
+    path('change/', TeamView.as_view()),
+    path('<int:league_id>/', TeamGetView.as_view()),
 ]
