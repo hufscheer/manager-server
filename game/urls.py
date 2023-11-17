cb5bcd2ce8b71ee3b94c538bcf93a1a8189ed25b
leehjhjhj <tsukiakarii@naver.com>
2023-11-17 23:43:44 +0900

    feat: 게임 수정 url 추가 #57

--- game/urls.py
+++ game/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from game.presentation import GameView, GameTeamView
+from game.presentation import GameView, GameTeamView, GameChangeView
 
 app_name = 'game'
 
 urlpatterns = [
     path('<int:league_id>/', GameView.as_view()),
-    path('team/<int:game_id>/', GameTeamView.as_view())
+    path('team/<int:game_id>/', GameTeamView.as_view()),
+    path('change/<int:game_id>/', GameChangeView.as_view())
 ]
