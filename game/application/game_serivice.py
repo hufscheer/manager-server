85e00e3c1145b68bed824ce73bea895382716d5c
leehjhjhj <tsukiakarii@naver.com>
2023-11-17 23:43:22 +0900

    feat: 게임 수정 서비스 추가 #57

--- game/application/game_serivice.py
+++ game/application/game_serivice.py
@@ -1,7 +1,12 @@
-from game.domain import GameRepository
+from game.domain import GameRepository, Game
 from accounts.domain import Member
-from game.serializers import GameRequestSerializer, GameSaveSerializer
+from game.serializers import (
+                    GameRequestSerializer,
+                    GameSaveSerializer,
+                    GameChangeRequestSerializer,
+                )
 from league.domain import LeagueRepository
+from django.core.exceptions import PermissionDenied
 
 class GameService:
     def __init__(self, game_repository: GameRepository, league_repository: LeagueRepository, *args, **kwargs):
@@ -19,3 +24,14 @@ class GameService:
         game_save_serialzier = GameSaveSerializer(data=game_data)
         game_save_serialzier.is_valid(raise_exception=True)
         game_save_serialzier.save()
+    
+    def change_game(self, game_id: int, request_data, user_data: Member):
+        game: Game = self._game_repository.find_game_by_id(game_id)
+        if game.administrator_id != user_data.id:
+            raise PermissionDenied
+        game_change_request_serializer = GameChangeRequestSerializer(data=request_data)
+        game_change_request_serializer.is_valid(raise_exception=True)
+        game_change_data = game_change_request_serializer.validated_data
+        game_save_serializer = GameSaveSerializer(game, data=game_change_data, partial=True)
+        game_save_serializer.is_valid(raise_exception=True)
+        game_save_serializer.save()
