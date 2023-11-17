bede13ee6a0c12bbeb2f6ca1cd090d56018e2cd7
leehjhjhj <tsukiakarii@naver.com>
2023-11-17 17:15:22 +0900

    feat: 쿼터 조회 서비스 생성 #51

--- sport/application/sport_service.py
+++ sport/application/sport_service.py
@@ -1,5 +1,5 @@
 from sport.domain import SportRepository
-from sport.serializers import SportsNameResponseSerializer
+from sport.serializers import SportsNameResponseSerializer, SportsQuarterResponseSerializer
 
 class SportService:
     def __init__(self, sport_repository: SportRepository, *args, **kwargs):
@@ -9,3 +9,8 @@ class SportService:
         sports = self._sport_repository.find_all_sport()
         sports_name_response_serializer = SportsNameResponseSerializer(sports, many=True)
         return sports_name_response_serializer.data
+    
+    def get_all_quarter_list(self, sport_id: int):
+        quarter_list = self._sport_repository.find_all_quarter_by_sport_id(sport_id)
+        sports_quarter_response_serializer = SportsQuarterResponseSerializer(quarter_list, many=True)
+        return sports_quarter_response_serializer.data
