dcdd8d393b256bbb76d2072797b939dc423d49f4
leehjhjhj <tsukiakarii@naver.com>
2023-11-20 16:23:12 +0900

    feat: 라인업전제조회 view 추가 #64

--- game/presentation/__init__.py
+++ game/presentation/__init__.py
@@ -2,3 +2,4 @@ from .game_view import GameView
 from .game_team_view import GameTeamView
 from .game_change_view import GameChangeView
 from .game_team_player_view import GameTeamPlayerView
+from .game_team_player_get_view import GameTeamPlayerGetView
