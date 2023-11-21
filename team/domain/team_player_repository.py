8af01132db8189b44c1ae1a1f6b16b8d3b2f275b
leehjhjhj <tsukiakarii@naver.com>
2023-11-21 19:52:56 +0900

    feat: 팀선수 조회관련 레포 추가 #73

--- team/domain/team_player_repository.py
+++ team/domain/team_player_repository.py
@@ -1,5 +1,5 @@
 from team.domain import TeamPlayer
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 class TeamPlayerRepository:
     def save_team_player(self, team_player: TeamPlayer):
@@ -13,3 +13,6 @@ class TeamPlayerRepository:
 
     def find_team_player_with_team_by_id(self, team_player_id: int):
         return get_object_or_404(TeamPlayer.objects.select_related('team'), id=team_player_id)
+    
+    def find_team_player_by_team_id(self, team_id: int):
+        return get_list_or_404(TeamPlayer, team_id=team_id)
