037c560c4b9365fcc01742d8112bc6f34321788f
leehjhjhj <tsukiakarii@naver.com>
2023-11-21 19:53:20 +0900

    chore: team_player_get 관련 레포 url 추가 #73

--- team/urls.py
+++ team/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from team.presentation import TeamView, TeamGetView, TeamPlayerView
+from team.presentation import TeamView, TeamGetView, TeamPlayerView, TeamPlayerGetView
 
 app_name = 'team'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('change/', TeamView.as_view()),
     path('<int:league_id>/', TeamGetView.as_view()),
     path('<int:team_id>/player/', TeamPlayerView.as_view()),
+    path('<int:team_id>/player/all/', TeamPlayerGetView.as_view()),
     path('player/<int:team_player_id>/', TeamPlayerView.as_view())
 ]
