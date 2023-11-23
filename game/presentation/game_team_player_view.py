8ee25b293a91236296d1b646f6b10ba87c4dbf26
leehjhjhj <tsukiakarii@naver.com>
2023-11-23 15:13:31 +0900

    refactor: 게임팀플레이어 뷰 의존성 변경 #75

--- game/presentation/game_team_player_view.py
+++ game/presentation/game_team_player_view.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from django.core.exceptions import PermissionDenied
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from accounts.domain import IsAdminUser
 from game.containers import GameContainer
@@ -12,12 +11,12 @@ class GameTeamPlayerView(APIView):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._game_team_serivice = GameContainer.game_team_serivice()
+        self._game_team_player_service = GameContainer.game_team_player_service()
 
     def post(self, request, game_team_id: int):
-        self._game_team_serivice.register_game_team_player(game_team_id, request.data)
+        self._game_team_player_service.register_game_team_player(game_team_id, request.data)
         return Response(status=status.HTTP_201_CREATED)
     
     def put(self, request, game_team_id: int):
-        self._game_team_serivice.change_game_team_player(game_team_id, request.data)
+        self._game_team_player_service.change_game_team_player(game_team_id, request.data)
         return Response(status=status.HTTP_200_OK)
