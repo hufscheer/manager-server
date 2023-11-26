0345830eb3cf837bb7c730c0a88e74dc75aa14e2
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 14:28:34 +0900

    feat: 댓글 블락 관련 레포 로직 작성  #89

--- report/domain/comment_repository.py
+++ report/domain/comment_repository.py
@@ -1,6 +1,12 @@
 from report.domain import Comment
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 class CommentRepository:
     def find_is_blocked_comments(self):
         return get_list_or_404(Comment.objects.order_by('-created_at'), is_blocked=True)
+    
+    def find_comment_by_id(self, comment_id: int):
+        return get_object_or_404(Comment, id=comment_id)
+    
+    def save_comment(self, comment: Comment):
+        comment.save()
