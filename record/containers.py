5f365bd40ed4f361610d7e8a072237e7fa3851c8
leehjhjhj <tsukiakarii@naver.com>
2023-11-26 13:56:13 +0900

    chore: 레코드 컨테이너 및 url 설정 #86

--- record/containers.py
+++ record/containers.py
@@ -1,5 +1,14 @@
 from dependency_injector import containers, providers
+from record.domain import RecordRepository
+from record.application import RecordService
+from game.domain import GameRepository
 
 class RecordContainer(containers.DeclarativeContainer):
-    pass
+    record_repository = providers.Factory(RecordRepository)
+    game_repository = providers.Factory(GameRepository)
+    record_service = providers.Factory(
+        RecordService,
+        record_repository=record_repository,
+        game_repository=game_repository,
+    )
 
