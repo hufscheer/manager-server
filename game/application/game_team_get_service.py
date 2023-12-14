4723e32ba92b97d6f7567fec70f749be54d61a97
leehjhjhj <tsukiakarii@naver.com>
2023-12-14 20:09:34 +0900

    feat: 게임팀 상세조회 관련 get service 로직 추가 #97

--- game/application/game_team_get_service.py
+++ game/application/game_team_get_service.py
@@ -1,5 +1,5 @@
-from game.domain import GameRepository, GameTeamPlayer
-from game.serializers import GameTeamPlayerGetSerializer
+from game.domain import GameRepository, GameTeamPlayer, GameTeam
+from game.serializers import GameTeamPlayerGetSerializer, GameTeamInfoSerializer
 
 class GameTeamGetService:
     def __init__(self, game_repository: GameRepository, *args, **kwargs):
@@ -9,3 +9,8 @@ class GameTeamGetService:
         game_team_players: list[GameTeamPlayer] = self._game_repository.find_game_team_players_by_game_team_id(game_team_id)
         game_team_player_get_serializer = GameTeamPlayerGetSerializer(game_team_players, many=True)
         return game_team_player_get_serializer.data
+    
+    def get_game_teams_info(self, game_id: int):
+        game_teams: list[GameTeam] = self._game_repository.find_game_teams_with_team_by_game_id(game_id)
+        game_team_info_serializer = GameTeamInfoSerializer(game_teams, many=True)
+        return game_team_info_serializer.data
