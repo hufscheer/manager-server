166c2a3b473dd6ec1b9de751f57839328a13ec69
leehjhjhj <tsukiakarii@naver.com>
2023-12-15 22:25:00 +0900

    refactor: 어드민용 게임 상세 조회 url 변경 #94

--- game/urls.py
+++ game/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('<int:league_id>/', GameView.as_view()),
     path('change/<int:game_id>/', GameChangeView.as_view()),
     path('score/', GameScoreView.as_view()),
-    path('info/<int:game_id>/', GameGetView.as_view()),
+    path('<int:game_id>/info/', GameGetView.as_view()),
     path('team/<int:game_id>/', GameTeamView.as_view()),
     path('<int:game_id>/team/', GameTeamGetView.as_view()),
     path('team/<int:game_team_id>/player/', GameTeamPlayerView.as_view()),
