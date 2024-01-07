d7ff999cf00c0a4189b4018cdf6dc662e1174d19
leehjhjhj <tsukiakarii@naver.com>
2024-01-07 16:09:12 +0900

    refactor: team exception시 code와 teamName까지 같이 response

--- utils/exceptions/exception_handler.py
+++ utils/exceptions/exception_handler.py
@@ -1,11 +1,12 @@
 from rest_framework.views import exception_handler
-from utils.exceptions.team_exceptions import S3UploadError, TeamSaveError
+from utils.exceptions.team_exceptions import S3UploadError, TeamSaveError, EmptyLogoError
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if response is not None:
-        if isinstance(exc, (S3UploadError, TeamSaveError)):
+        if isinstance(exc, (S3UploadError, TeamSaveError, EmptyLogoError)):
             response.data['teamName'] = exc.team_name
+            response.data['code'] = exc.default_code
 
     return response
