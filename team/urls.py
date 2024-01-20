cca06a729a953bc816b2341274a791ead76b6ae8
leehjhjhj <tsukiakarii@naver.com>
2024-01-20 09:55:44 +0900

    refactor: 팀 서비스, 팀 등록 서비스 분리

--- team/urls.py
+++ team/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from team.presentation import TeamView, TeamGetView, TeamPlayerView, TeamPlayerGetView
+from team.presentation import TeamView, TeamGetView, TeamPlayerView, TeamPlayerGetView, TeamRegisterView
 
 app_name = 'team'
 
 urlpatterns = [
-    path('register/<int:league_id>/', TeamView.as_view()),
+    path('register/<int:league_id>/', TeamRegisterView.as_view()),
     path('<int:team_id>/change/', TeamView.as_view()),
     path('<int:league_id>/', TeamGetView.as_view()),
     path('<int:team_id>/player/', TeamPlayerView.as_view()),
