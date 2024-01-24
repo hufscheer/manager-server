a243661925b2a689d6e24a82073ebe155c8b8948
leehjhjhj <tsukiakarii@naver.com>
2024-01-24 19:10:34 +0900

    refactor: urls.py 새로운 스펙에 따라 변경

--- game/urls.py
+++ game/urls.py
@@ -5,19 +5,19 @@ from game.presentation import (
                                 GameScoreView,
                                 GameGetView,
                                 GameTeamView,
-                                GameTeamPlayerView,
-                                GameTeamPlayerGetView,
+                                LineupPlayerView,
+                                LineupPlayerGetView,
                                 GameTeamGetView,
                             )
 app_name = 'game'
 
 urlpatterns = [
     path('<int:league_id>/', GameView.as_view()),
-    path('change/<int:game_id>/', GameChangeView.as_view()),
+    path('<int:game_id>/change/', GameChangeView.as_view()),
     path('score/', GameScoreView.as_view()),
     path('<int:game_id>/info/', GameGetView.as_view()),
-    path('team/<int:game_id>/', GameTeamView.as_view()),
+    #path('team/<int:game_id>/', GameTeamView.as_view()),
     path('<int:game_id>/team/', GameTeamGetView.as_view()),
-    path('team/<int:game_team_id>/player/', GameTeamPlayerView.as_view()),
-    path('team/<int:game_team_id>/player/all/', GameTeamPlayerGetView.as_view()),
+    path('team/<int:game_team_id>/lineup-player/', LineupPlayerView.as_view()),
+    path('team/<int:game_team_id>/lineup-player/all/', LineupPlayerGetView.as_view()),
 ]
