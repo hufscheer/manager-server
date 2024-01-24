52f51be82ce65e4bf989750618206e94a2ce4ac7
leehjhjhj <tsukiakarii@naver.com>
2024-01-24 19:33:23 +0900

    refactor: 전체 url 's' 추가 및 record -> timelime로 수정

--- sports_live/urls.py
+++ sports_live/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
-    path('game/', include("game.urls")),
-    path('league/', include("league.urls")),
-    path('report/', include("report.urls")),
-    path('team/', include("team.urls")),
-    path('record/', include("record.urls")),
-    path('sport/', include("sport.urls")),
+    path('games/', include("game.urls")),
+    path('leagues/', include("league.urls")),
+    path('reports/', include("report.urls")),
+    path('teams/', include("team.urls")),
+    path('timelines/', include("record.urls")),
+    path('sports/', include("sport.urls")),
 ]
