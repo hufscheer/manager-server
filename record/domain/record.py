862031a74e687b127949dd098653a485b89f0fe5
leehjhjhj <tsukiakarii@naver.com>
2024-01-24 19:17:08 +0900

    refactor: game_team_player -> lineup_player로 명세 업데이트

--- record/domain/record.py
+++ record/domain/record.py
@@ -6,7 +6,7 @@ class Record(models.Model):
     id = models.BigAutoField(primary_key=True)
     game = models.ForeignKey(Game, models.DO_NOTHING)
     game_team = models.ForeignKey(GameTeam, models.DO_NOTHING)
-    lineup_players = models.ForeignKey(LineupPlayer, models.DO_NOTHING)
+    lineup_player = models.ForeignKey(LineupPlayer, models.DO_NOTHING)
     score = models.IntegerField()
     scored_quarter = models.ForeignKey(Quarter, models.DO_NOTHING)
     scored_at = models.DateTimeField(auto_now_add=True)
--- record/serializers/record_serializer.py
+++ record/serializers/record_serializer.py
@@ -3,10 +3,10 @@ from record.domain import Record
 
 class RecordRequestSerializer(serializers.ModelSerializer):
     gameTeamId = serializers.IntegerField(source='game_team_id')
-    gameTeamPlayerId = serializers.IntegerField(source='game_team_player_id')
+    lineupPlayerId = serializers.IntegerField(source='lineup_player_id')
     score = serializers.IntegerField()
     quarterId = serializers.IntegerField(source='quarter_id')
 
     class Meta:
         model = Record
-        fields = ('gameTeamId', 'gameTeamPlayerId', 'score', 'quarterId')
+        fields = ('gameTeamId', 'lineupPlayerId', 'score', 'quarterId')
