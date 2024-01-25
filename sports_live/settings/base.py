8040997990dc2c3cd2117984d9d8c157aa419ea3
leehjhjhj <tsukiakarii@naver.com>
2024-01-25 22:51:06 +0900

    chore: static 경로를 위한 BASE_DIR 수정

--- sports_live/settings/base.py
+++ sports_live/settings/base.py
@@ -4,7 +4,7 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
