fc7114ac9e95cca1b8c37f29de8ea0e10d791d97
leehjhjhj <tsukiakarii@naver.com>
2024-01-25 17:53:51 +0900

    fix: serializer source name 변경

--- report/domain/report_repository.py
+++ report/domain/report_repository.py
@@ -3,4 +3,4 @@ from report.domain import Report
 
 class ReportRepository:
     def find_pending_reports_with_cheer_talk(self):
-        return get_list_or_404(Report.objects.select_related('cheertalk').order_by('-reported_at'), state='PENDING')
+        return get_list_or_404(Report.objects.select_related('cheer_talk').order_by('-reported_at'), state='PENDING')
