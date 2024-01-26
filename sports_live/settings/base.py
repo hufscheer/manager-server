64a769a18b155eb7813d1faa0a6760e37ffeaf39
leehjhjhj <tsukiakarii@naver.com>
2024-01-26 11:02:18 +0900

    chore: 환경 별 static 경로 변경

--- sports_live/settings/base.py
+++ sports_live/settings/base.py
@@ -4,7 +4,7 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -160,8 +160,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'data/static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
