89bf7ac4f404902bca97d00795b06c55d5f8ade4
leehjhjhj <tsukiakarii@naver.com>
2024-02-17 15:29:15 +0900

    add: number 추가

--- team/domain/team_player.py
+++ team/domain/team_player.py
@@ -5,7 +5,8 @@ class LeagueTeamPlayer(models.Model):
     league_team = models.ForeignKey('LeagueTeam', models.DO_NOTHING)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
-
+    number = models.IntegerField(null=True)
+    
     class Meta:
         managed = False
         db_table = 'league_team_players'
