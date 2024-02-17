1f05a08d41eae8738017d461011f7d3f0f537ee9
leehjhjhj <tsukiakarii@naver.com>
2024-02-17 15:28:46 +0900

    refactor: scored_record, replacement_record 추가

--- record/domain/record.py
+++ record/domain/record.py
@@ -2,15 +2,38 @@ from django.db import models
 from game.domain import Game, GameTeam, LineupPlayer
 from sport.domain import Quarter
 
-class Record(models.Model): 
+class Record(models.Model):
+    RECORD_TYPE_CHOICES = (
+        ('SCORE', 'score'),
+        ('REPLACEMENT', 'replacement')
+    )
     id = models.BigAutoField(primary_key=True)
     game = models.ForeignKey(Game, models.DO_NOTHING)
     game_team = models.ForeignKey(GameTeam, models.DO_NOTHING)
-    lineup_player = models.ForeignKey(LineupPlayer, models.DO_NOTHING)
-    score = models.IntegerField()
-    scored_quarter = models.ForeignKey(Quarter, models.DO_NOTHING)
-    scored_at = models.DateTimeField(auto_now_add=True)
+    recorded_quarter = models.ForeignKey(Quarter, models.DO_NOTHING)
+    recorded_at = models.DateTimeField(auto_now_add=True)
+    record_type = models.CharField(max_length=255, choices=RECORD_TYPE_CHOICES, null=True)
 
     class Meta:
         managed = False
         db_table = 'records'
+
+class ScoreRecord(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    record = models.ForeignKey(Record, models.DO_NOTHING)
+    lineup_player = models.ForeignKey(LineupPlayer, models.DO_NOTHING)
+    score = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'score_records'
+
+class ReplacementRecord(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    record = models.ForeignKey(Record, models.DO_NOTHING)
+    origin_lineup_player = models.ForeignKey(LineupPlayer, models.DO_NOTHING)
+    replaced_lineup_player = models.ForeignKey(LineupPlayer, models.DO_NOTHING)
+    
+    class Meta:
+        managed = False
+        db_table = 'replacement_records'
