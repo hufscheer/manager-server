9fcdb347bddb7a4226002f232df4e0816fb5e6b6
leehjhjhj <tsukiakarii@naver.com>
2024-02-23 03:32:02 +0900

    test: 테스트를 위한 fake dto 추가

--- src/sportslive/team/dto/team_dto.py
+++ src/sportslive/team/dto/team_dto.py
@@ -9,7 +9,19 @@ class TeamRequestDTO:
     @property
     def logos(self):
         return self._data.getlist('logos')
-    
+
+class FakeTeamRequestDTO:
+    def __init__(self, request_data):
+        self._data = request_data
+
+    @property
+    def names(self):
+        return self._data.get('names')
+
+    @property
+    def logos(self):
+        return self._data.get('logos')
+
 class TeamMakeDTO:
     def __init__(self, name: str, logo) -> None:
         self.name = name
