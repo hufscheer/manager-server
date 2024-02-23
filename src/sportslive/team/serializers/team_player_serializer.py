ea7d21f8655d2add2d63c752a401a5ea5ce5bc69
leehjhjhj <tsukiakarii@naver.com>
2024-02-23 16:37:16 +0900

    add: team player에 number 필드 추가

--- src/sportslive/team/serializers/team_player_serializer.py
+++ src/sportslive/team/serializers/team_player_serializer.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 from team.domain import LeagueTeamPlayer
 
 class TeamPlayerRegisterRequestSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(allow_null=True, allow_blank=True, required=False)
 
     class Meta:
         model = LeagueTeamPlayer
-        fields = ('name', 'description')
+        fields = ('name', 'description', 'number')
 
 class TeamPlayerChangeRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = LeagueTeamPlayer
-        fields = ('name', 'description')
+        fields = ('name', 'description', 'number')
 
 class TeamPlayersResponseSerialier(serializers.ModelSerializer):
 
     class Meta:
         model = LeagueTeamPlayer
-        fields = ('id', 'name', 'description')
+        fields = ('id', 'name', 'description', 'number')
