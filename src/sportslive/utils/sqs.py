9cc08cf7ba424625a02241897fa86144e4fd75da
leehjhjhj <tsukiakarii@naver.com>
2024-02-25 13:08:29 +0900

    HOTFIX: sqs에 리젼 이름 명시

--- src/sportslive/utils/sqs.py
+++ src/sportslive/utils/sqs.py
@@ -14,7 +14,7 @@ class SqsConnect(AbstractSqsConnect):
     _sqs_queue_url = config('SQS_QUEUE_URL')
     
     def __init__(self):
-        self._sqs_conn = boto3.client('sqs', aws_access_key_id=self._aws_access_key, aws_secret_access_key=self._aws_secret_key)
+        self._sqs_conn = boto3.client('sqs', region_name='ap-northeast-2', aws_access_key_id=self._aws_access_key, aws_secret_access_key=self._aws_secret_key)
 
     def send_message_to_sqs(self, s3_key):
         response = self._sqs_conn.send_message(
