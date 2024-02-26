9a8e054ceeda910ca38a8f927f3ada8d58c3c928
leehjhjhj <tsukiakarii@naver.com>
2024-02-26 16:17:42 +0900

    delete: 쓰이지 않은 game_team 생성 로직 삭제

--- src/sportslive/game/application/game_team_serivce.py
+++ src/sportslive/game/application/game_team_serivce.py
@@ -11,20 +11,6 @@ class GameTeamService:
     def __init__(self, game_repository: GameRepository, team_repository: TeamRepository, *args, **kwargs):
         self._game_repository = game_repository
         self._team_repository = team_repository
-
-    def create_game_team(self, request_data, game_id: int):
-        game_team_request_serializer = GameTeamRequestSerializer(data=request_data)
-        game_team_request_serializer.is_valid(raise_exception=True)
-        game_team_data = game_team_request_serializer.validated_data
-        team_ids: list = game_team_data.get('team_ids')
-
-        game = self._game_repository.find_game_by_id(game_id)
-
-        for team_id in team_ids:
-            team = self._team_repository.find_team_by_id(team_id)
-            game_team_save_serializer = GameTeamSaveSerializer(data=self._GameTeamDto(game.id, team.id))
-            game_team_save_serializer.is_valid(raise_exception=True)
-            game_team_save_serializer.save()
     
     def change_score(self, request_data: dict):
         game_score_change_serializer = GameScoreChangeSerializer(data=request_data)
