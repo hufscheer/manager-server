90ffce964775427179d94e8c2688d3e0811730ee
leehjhjhj <tsukiakarii@naver.com>
2024-03-01 13:14:41 +0900

    refactor: record create service 로 의존성 이름변경

--- src/sportslive/record/containers.py
+++ src/sportslive/record/containers.py
@@ -1,13 +1,13 @@
 from dependency_injector import containers, providers
 from record.domain import RecordRepository
-from record.application import RecordService
+from record.application import RecordCreateService
 from game.domain import GameRepository
 
 class RecordContainer(containers.DeclarativeContainer):
     record_repository = providers.Factory(RecordRepository)
     game_repository = providers.Factory(GameRepository)
-    record_service = providers.Factory(
-        RecordService,
+    record_create_service = providers.Factory(
+        RecordCreateService,
         record_repository=record_repository,
         game_repository=game_repository,
     )
