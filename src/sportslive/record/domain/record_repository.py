ec3cb5895af3f184cdf40cd67218ca44bb480fb5
leehjhjhj <tsukiakarii@naver.com>
2024-03-03 17:42:00 +0900

    feat: 스코어, 교체 타임라인 찾기 로직 추가

--- src/sportslive/record/domain/record_repository.py
+++ src/sportslive/record/domain/record_repository.py
@@ -1,4 +1,18 @@
+from django.shortcuts import get_object_or_404
+from record.domain import Record, ReplacementRecord, ScoreRecord
 
 class RecordRepository:
-    def save_record(self, record):
+    def save_record(self, record: Record):
         record.save()
+
+    def find_record_by_id(self, record_id: int):
+        return get_object_or_404(Record, id=record_id)
+
+    def find_record_by_id_with_game_team(self, record_id: int):
+        return get_object_or_404(Record.objects.select_related('game_team'), id=record_id)
+
+    def find_score_record_by_id(self, score_record_id: int):
+        return get_object_or_404(ScoreRecord, id=score_record_id)
+    
+    def find_replacement_record_by_id(self, replaement_record_id: int):
+        return get_object_or_404(ReplacementRecord, id=replaement_record_id)
