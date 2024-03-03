1090d394f2a4e71a8d3be1d05d503aed65d9b4cf
leehjhjhj <tsukiakarii@naver.com>
2024-03-03 17:40:46 +0900

    feat: change serialzier 추가

--- src/sportslive/record/serializers/record_serializer.py
+++ src/sportslive/record/serializers/record_serializer.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
 
 class ScoreRecordRequestSerializer(serializers.Serializer):
+    recordedAt = serializers.DateTimeField(source='recorded_at')
+    gameTeamId = serializers.IntegerField(source='game_team_id')
+    scoreLineupPlayerId = serializers.IntegerField(source='score_lineup_player_id')
+    recordedQuarterId = serializers.IntegerField(source='recorded_quarter_id')
+    score = serializers.IntegerField()
+
+class ReplacementRecordRequestSerializer(serializers.Serializer):
+    recordedAt = serializers.DateTimeField(source='recorded_at')
+    gameTeamId = serializers.IntegerField(source='game_team_id')
+    recordedQuarterId = serializers.IntegerField(source='recorded_quarter_id')
+    originLineupPlayerId = serializers.IntegerField(source='origin_lineup_player_id')
+    replacedLineupPlayerId = serializers.IntegerField(source='replaced_lineup_player_id')
+
+class ScoreRecordChangeRequestSerializer(serializers.Serializer):
+    recordedAt = serializers.IntegerField(source='recorded_at')
     gameTeamId = serializers.IntegerField(source='game_team_id')
     recordedQuarterId = serializers.IntegerField(source='recorded_quarter_id')
     scoreLineupPlayerId = serializers.IntegerField(source='score_lineup_player_id')
     score = serializers.IntegerField()
 
-class ReplacementRecordRequestSerializer(serializers.Serializer):
+class ReplacementRecordChangeRequestSerializer(serializers.Serializer):
+    recordedAt = serializers.IntegerField(source='recorded_at')
     gameTeamId = serializers.IntegerField(source='game_team_id')
     recordedQuarterId = serializers.IntegerField(source='recorded_quarter_id')
     originLineupPlayerId = serializers.IntegerField(source='origin_lineup_player_id')
