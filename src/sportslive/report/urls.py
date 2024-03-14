7efc645e1f768b8d597a03a8d59d34832c87b4d8
leehjhjhj <tsukiakarii@naver.com>
2024-03-15 00:50:05 +0900

    report: 로직 변경에 따른 cheer_talk_id -> report_id

--- src/sportslive/report/urls.py
+++ src/sportslive/report/urls.py
@@ -5,6 +5,6 @@ app_name = 'report'
 
 urlpatterns = [
     path('', ReportView.as_view()),
-    path('cheer-talk/<int:cheer_talk_id>/', BlockCheerTalkView.as_view()),
+    path('cheer-talk/<int:report_id>/', BlockCheerTalkView.as_view()),
     path('invalid/<int:report_id>/', InvalidReportView.as_view()),
 ]
