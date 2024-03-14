fbcc76698940ec40e8b864fc6048a2f7238bf141
leehjhjhj <tsukiakarii@naver.com>
2024-03-15 01:07:07 +0900

    HOTFIX: is_block이 true가 없더라도 404 뜨지 않게 변경

--- src/sportslive/report/domain/cheer_talk_repository.py
+++ src/sportslive/report/domain/cheer_talk_repository.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 
 class CheerTalkRepository:
     def find_is_blocked_cheer_talks(self):
-        return get_list_or_404(CheerTalk.objects.order_by('-created_at'), is_blocked=True)
+        return CheerTalk.objects.order_by('-created_at').filter(is_blocked=True)
     
     def find_cheer_talk_by_id(self, cheer_talk_id: int):
         return get_object_or_404(CheerTalk, id=cheer_talk_id)
