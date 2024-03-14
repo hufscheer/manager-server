9777e26aaa418c743e6142e1dc5e95fb42284ff5
leehjhjhj <tsukiakarii@naver.com>
2024-03-15 00:19:47 +0900

    chore: invalid view url추가

--- src/sportslive/report/urls.py
+++ src/sportslive/report/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from report.presentation import ReportView, BlockCheerTalkView
+from report.presentation import ReportView, BlockCheerTalkView, InvalidReportView
 
 app_name = 'report'
 
 urlpatterns = [
     path('', ReportView.as_view()),
-    path('cheer-talk/<int:cheer_talk_id>/', BlockCheerTalkView.as_view())
+    path('cheer-talk/<int:cheer_talk_id>/', BlockCheerTalkView.as_view()),
+    path('invalid/<int:report_id>/', InvalidReportView.as_view()),
 ]
