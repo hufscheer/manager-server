245b2b3c06735b84f78b98a96b250947596ad676
leehjhjhj <tsukiakarii@naver.com>
2024-03-19 00:18:05 +0900

    feat: 쿠키에서 엑세스 토큰을 헤더에 추가하는 커스텀 미들웨어 추가

--- src/sportslive/sports_live/settings/base.py
+++ src/sportslive/sports_live/settings/base.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'sports_live.middleware.CustomJWTAuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
