86096a36f3766c968134a63c005ea9a2999abefb
leehjhjhj <tsukiakarii@naver.com>
2024-03-18 12:13:34 +0900

    test: main get 관련 test 추가

--- src/tests/test_league_get.py
+++ src/tests/test_league_get.py
@@ -4,6 +4,7 @@ from .fixture import load_sql_fixture
 from league.containers import LeagueContainer
 from league.domain import LeagueSport, League
 from django.core.exceptions import PermissionDenied
+from freezegun import freeze_time
 
 class TestLeagueGet:
 
@@ -12,9 +13,17 @@ class TestLeagueGet:
         self._league_get_service = LeagueContainer.league_get_service()
 
     @pytest.mark.django_db
+    @freeze_time("2024-03-18")
     def test_get_league_list(self, load_sql_fixture, dependency_fixture):
         member = Member.objects.get(id=1)
         response = self._league_get_service.get_leagues(member)
         assert len(response['scheduled']) == 2
+
+    @pytest.mark.django_db
+    @freeze_time("2024-03-21")
+    def test_get_main_list(self, load_sql_fixture, dependency_fixture):
+        member = Member.objects.get(id=1)
+        response = self._league_get_service.get_main(member)
+        assert len(response[0].get("playingGameData")) == 1
         
         
