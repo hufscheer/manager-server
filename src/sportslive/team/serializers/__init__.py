e08762b02096b0e8d16b1395846c77f86f0daee8
leehjhjhj <tsukiakarii@naver.com>
2024-03-20 18:53:11 +0900

    feat: 팀 등록시 ids 반환 시리얼라이저 추가

--- src/sportslive/team/serializers/__init__.py
+++ src/sportslive/team/serializers/__init__.py
@@ -2,6 +2,7 @@ from .team_serializer import (
                                 TeamRegisterRequestSerializer,
                                 TeamSaveSerializer,
                                 TeamChangeRequestSerializer,
+                                TeamRegisterResponseSerializer,
                             )
 from .team_get_serializer import TeamGetSerializer
 from .team_player_serializer import (
