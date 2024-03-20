a5f12d01be1ee8651041aa96b82681d66ac7734c
leehjhjhj <tsukiakarii@naver.com>
2024-03-21 02:32:29 +0900

    HOTFIX: '/' 추가

--- src/sportslive/team/urls.py
+++ src/sportslive/team/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('<int:team_id>/change/', TeamView.as_view()),
     path('<int:league_id>/', TeamGetView.as_view()),
     path('<int:team_id>/player/', TeamPlayerCreateView.as_view()),
-    path('<int:team_id>/player/<int:team_player_id>', TeamPlayerUpdateDeleteView.as_view()),
+    path('player/<int:team_player_id>/', TeamPlayerUpdateDeleteView.as_view()),
     path('<int:team_id>/player/all/', TeamPlayerGetView.as_view()),
 ]
