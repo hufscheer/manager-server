c6b1f840516665c2d9ba1b0fa901ed993d71d7a5
leehjhjhj <tsukiakarii@naver.com>
2024-03-21 01:18:54 +0900

    refactor: extra_record_id 삭제와 url 파라미터 순서 변경

--- src/sportslive/record/urls.py
+++ src/sportslive/record/urls.py
@@ -9,6 +9,6 @@ app_name = 'record'
 
 urlpatterns = [
     path('create/<str:record_type>/<int:game_id>/', RecordCreateView.as_view()),
-    path('change/<int:record_id>/<int:extra_record_id>/<str:record_type>/', RecordChangeView.as_view()),
-    path('delete/<str:record_type>/<int:extra_record_id>/<int:game_id>/', RecordDeleteView.as_view()),
+    path('change/<int:record_id>/<str:record_type>/', RecordChangeView.as_view()),
+    path('delete/<int:record_id>/<str:record_type>/', RecordDeleteView.as_view()),
 ]
