212bff36a8cc4243623dd8ff455d32f38fa81efc
leehjhjhj <tsukiakarii@naver.com>
2024-03-21 11:01:29 +0900

    refacator: get service 분리 작업

--- src/sportslive/report/application/report_service.py
+++ src/sportslive/report/application/report_service.py
@@ -1,26 +1,10 @@
 from report.domain import ReportRepository, CheerTalkRepository, Report, CheerTalk
-from report.serializers import ReportResponseSerializer
-from game.domain import GameRepository, GameTeam
-from accounts.domain import Member
-from typing import List, Union
+from game.domain import GameRepository
 
 class ReportService:
-    def __init__(self, report_repository: ReportRepository, cheer_talk_repository: CheerTalkRepository, game_repository: GameRepository):
+    def __init__(self, report_repository: ReportRepository, cheer_talk_repository: CheerTalkRepository):
         self._report_repository = report_repository
         self._cheer_talk_repository = cheer_talk_repository
-        self._game_repository = game_repository
-
-    def get_report_info(self, user_data: Member):
-        pending_reports: list[Report] = self._report_repository.find_pending_reports_with_cheer_talk()
-        game_team_ids: list[int] = [report.cheer_talk.game_team_id for report in pending_reports]
-        pending_report_infos = self._get_report_info_object(game_team_ids, pending_reports, user_data)
-
-        blocked_cheer_talks: list[CheerTalk] = self._cheer_talk_repository.find_is_blocked_cheer_talks()
-        game_team_ids: list[int] = [cheer_talk.game_team_id for cheer_talk in blocked_cheer_talks]
-        blocked_cheer_talks_infos = self._get_report_info_object(game_team_ids, blocked_cheer_talks, user_data)
-
-        response_dto = self._ResponseDto(pending_report_infos, blocked_cheer_talks_infos)
-        return ReportResponseSerializer(response_dto).data
     
     def block_cheer_talk(self, report_id: int):
         report: Report = self._report_repository.find_report_by_id(report_id)
@@ -40,23 +24,3 @@ class ReportService:
         report: Report = self._report_repository.find_report_by_id(report_id)
         report.state = 'INVALID'
         self._report_repository.save_report(report)
-
-    def _get_report_info_object(self, game_team_ids: list[int], reports_or_cheer_talks: List[Union[CheerTalk, Report]], user_data: Member):
-        game_team_objects = self._game_repository.find_game_team_with_game_league_and_sport_by_ids(game_team_ids, user_data)
-        game_team_dict = {game_team.id: game_team for game_team in game_team_objects}
-        game_team_infos = [game_team_dict.get(game_team_id, None) for game_team_id in game_team_ids]
-        return [
-                self._ReportOrCheerTalkInfo(report_or_cheer_talk, game_info)
-                for report_or_cheer_talk, game_info in zip(reports_or_cheer_talks, game_team_infos)
-                if game_info is not None
-            ]
-    
-    class _ResponseDto:
-        def __init__(self, pending: list, blocked_cheer_talks_infos: list):
-            self.pending = pending
-            self.blocked_cheer_talks_infos = blocked_cheer_talks_infos
-
-    class _ReportOrCheerTalkInfo:
-        def __init__(self, report_or_cheer_talk, game_info: GameTeam):
-            self.report_or_cheer_talk = report_or_cheer_talk
-            self.game_info = game_info
--- src/sportslive/report/containers.py
+++ src/sportslive/report/containers.py
@@ -1,7 +1,7 @@
 from dependency_injector import containers, providers
 from report.domain import ReportRepository, CheerTalkRepository
 from game.domain import GameRepository
-from report.application import ReportService
+from report.application import ReportService, ReportGetService
 
 class ReportContainer(containers.DeclarativeContainer):
     report_repository = providers.Factory(ReportRepository)
@@ -9,6 +9,11 @@ class ReportContainer(containers.DeclarativeContainer):
     game_repository = providers.Factory(GameRepository)
     report_service = providers.Factory(
         ReportService,
+        report_repository=report_repository,
+        cheer_talk_repository=cheer_talk_repository
+    )
+    report_get_service = providers.Factory(
+        ReportGetService,
         report_repository=report_repository,
         cheer_talk_repository=cheer_talk_repository,
         game_repository=game_repository,
