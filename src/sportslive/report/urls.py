25049c8ff0f9f5436bb686c0161e3cfb027cb7c1
leehjhjhj <tsukiakarii@naver.com>
2024-03-21 11:01:55 +0900

    refactor: get 서비스 분리로 인한 url 경로 변경

--- src/sportslive/report/urls.py
+++ src/sportslive/report/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from report.presentation import ReportView, BlockCheerTalkView, InvalidReportView
+from report.presentation import ReportListView, BlockCheerTalkView, InvalidReportView
 
 app_name = 'report'
 
 urlpatterns = [
-    path('', ReportView.as_view()),
+    path('all/', ReportListView.as_view()),
     path('cheer-talk/<int:report_id>/', BlockCheerTalkView.as_view()),
     path('invalid/<int:report_id>/', InvalidReportView.as_view()),
 ]
