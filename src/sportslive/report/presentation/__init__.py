7b42e5d5b4d07ac2a38853eadd1e908bafd52118
leehjhjhj <tsukiakarii@naver.com>
2024-03-21 11:00:52 +0900

    refacator: get service 분리로 인한 get list 뷰로 변경

--- src/sportslive/report/presentation/__init__.py
+++ src/sportslive/report/presentation/__init__.py
@@ -1,3 +1,3 @@
-from .report_view import ReportView
+from .report_list_view import ReportListView
 from .block_cheertalk_view import BlockCheerTalkView
 from .invalid_report_view import InvalidReportView
--- src/sportslive/report/presentation/report_list_view.py
+++ src/sportslive/report/presentation/report_list_view.py
@@ -7,18 +7,18 @@ from report.containers import ReportContainer
 from drf_yasg.utils import swagger_auto_schema
 from report.serializers import ReportResponseSerializer
 
-class ReportView(APIView):
+class ReportListView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAdminUser]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._report_service = ReportContainer.report_service()
+        self._report_get_service = ReportContainer.report_get_service()
 
     @swagger_auto_schema(responses={"200": ReportResponseSerializer})
     def get(self, request):
         """
         신고 조회 API
         """
-        response = self._report_service.get_report_info(request.user)
+        response = self._report_get_service.get_report_info(request.user)
         return Response(response, status=status.HTTP_200_OK)
