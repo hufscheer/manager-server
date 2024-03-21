092906b9c93f03e2c9aefbd34f921919193b4839
leehjhjhj <tsukiakarii@naver.com>
2024-03-21 13:55:54 +0900

    refactor: change, delete시 record_type 삭제

--- src/sportslive/record/urls.py
+++ src/sportslive/record/urls.py
@@ -11,6 +11,6 @@ app_name = 'record'
 urlpatterns = [
     path('<int:record_id>/', RecordGetView.as_view()),
     path('create/<str:record_type>/<int:game_id>/', RecordCreateView.as_view()),
-    path('change/<int:record_id>/<str:record_type>/', RecordChangeView.as_view()),
-    path('delete/<int:record_id>/<str:record_type>/', RecordDeleteView.as_view()),
+    path('change/<int:record_id>/', RecordChangeView.as_view()),
+    path('delete/<int:record_id>/', RecordDeleteView.as_view()),
 ]
