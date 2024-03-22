f274e097aca11a9c29624c199c557fb13d57ea8b
leehjhjhj <tsukiakarii@naver.com>
2024-03-22 10:07:15 +0900

    feat: league team player를 game team id로 조회할 수 있는 서비스 로직 추가

--- src/sportslive/team/application/team_player_get_service.py
+++ src/sportslive/team/application/team_player_get_service.py
@@ -1,12 +1,25 @@
 from team.domain import TeamPlayerRepository, TeamRepository, LeagueTeamPlayer
 from team.serializers import TeamPlayersResponseSerialier
+from game.domain import GameRepository, GameTeam
 
 class TeamPlayerGetService:
-    def __init__(self, team_repository: TeamRepository, team_player_repository: TeamPlayerRepository):
+    def __init__(
+            self,
+            team_repository: TeamRepository,
+            team_player_repository: TeamPlayerRepository,
+            game_repository: GameRepository
+        ):
         self._team_player_repository = team_player_repository
         self._team_repository = team_repository
+        self._game_repository = game_repository
 
     def get_all_team_player(self, team_id: int):
         team_players: list[LeagueTeamPlayer] = self._team_player_repository.find_team_player_by_team_id(team_id)
+        team_player_data = TeamPlayersResponseSerialier(team_players, many=True)
+        return team_player_data.data
+    
+    def get_all_team_player_by_game_team(self, game_team_id: int):
+        game_team: GameTeam = self._game_repository.find_game_team_by_id(game_team_id)
+        team_players: list[LeagueTeamPlayer] = self._team_player_repository.find_team_player_by_team_id(game_team.league_team_id)
         team_player_data = TeamPlayersResponseSerialier(team_players, many=True)
         return team_player_data.data
