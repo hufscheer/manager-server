555c744ad35af824583afcfbdd3171185e021e1a
leehjhjhj <tsukiakarii@naver.com>
2024-03-24 00:17:05 +0900

    test: organizaion 확인, 시작시간 끝시간 검사 추가로 인한 테스트 추가

--- src/tests/test_league.py
+++ src/tests/test_league.py
@@ -4,6 +4,7 @@ from .fixture import load_sql_fixture
 from league.containers import LeagueContainer
 from league.domain import LeagueSport, League
 from django.core.exceptions import PermissionDenied
+from utils.exceptions.league_exception import CantEndAtFasterThanStartAt
 
 class TestLeague:
 
@@ -31,6 +32,24 @@ class TestLeague:
         assert LeagueSport.objects.get(league_id=3).sport_id == 1
 
     @pytest.mark.django_db
+    def test_create_fail_league(self, load_sql_fixture, dependency_fixture):
+        member = Member.objects.get(id=1)
+        request_data = {
+            "leagueData": {
+                "name": "푸드파이터대회",
+                "startAt": "2019-08-24T14:15:22Z",
+                "endAt": "2019-08-24T14:13:22Z",
+                "maxRound": 16,
+                "inProgressRound": 16
+            },
+            "sportData": [
+                1
+            ]
+        }
+        with pytest.raises(CantEndAtFasterThanStartAt):
+            self._league_service.register_league(request_data, member)
+
+    @pytest.mark.django_db
     def test_change_league(self, load_sql_fixture, dependency_fixture):
         member = Member.objects.get(id=1)
         request_data = {
@@ -55,7 +74,7 @@ class TestLeague:
 
     @pytest.mark.django_db
     def test_change_fail_league(self, load_sql_fixture, dependency_fixture):
-        member = Member.objects.get(id=2)
+        member = Member.objects.get(id=3)
         request_data = {
             "leagueData": {
                 "name": "푸파대회",
@@ -83,7 +102,7 @@ class TestLeague:
 
     @pytest.mark.django_db
     def test_cant_delete_league(self, load_sql_fixture, dependency_fixture):
-        member = Member.objects.get(id=2)
+        member = Member.objects.get(id=3)
         request_data = {
             "leagueId": 1
         }
