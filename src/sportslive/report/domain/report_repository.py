e072131fe293f979dd88cdd7320d1f20304dbd47
leehjhjhj <tsukiakarii@naver.com>
2024-03-24 21:11:24 +0900

    HOTFIX: 신고가 없으면 404 뜨지 않게 변경

--- src/sportslive/report/domain/report_repository.py
+++ src/sportslive/report/domain/report_repository.py
@@ -3,7 +3,7 @@ from report.domain import Report
 
 class ReportRepository:
     def find_pending_reports_with_cheer_talk(self):
-        return get_list_or_404(Report.objects.select_related('cheer_talk').order_by('-reported_at'), state='PENDING')
+        return Report.objects.select_related('cheer_talk').filter(state='PENDING').order_by('-reported_at')
     
     def find_report_by_id(self, report_id: int):
         return get_object_or_404(Report, id=report_id)
