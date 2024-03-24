93d5c97aef30fc543aa8696d88585a5ae7426c17
leehjhjhj <tsukiakarii@naver.com>
2024-03-24 10:33:34 +0900

    feat: youtube url 파싱 오류 추가

--- src/sportslive/utils/exceptions/game_exceptions.py
+++ src/sportslive/utils/exceptions/game_exceptions.py
@@ -3,3 +3,7 @@ from rest_framework.exceptions import APIException
 class CantDeleteGameError(APIException):
     status_code = 400
     default_detail = "현재 상태는 게임을 삭제할 수 없습니다."
+
+class CantParsingYoutubeUrl(APIException):
+    status_code = 400
+    default_detail = "유튜브 링크 형식이 잘못되었습니다."
