40d49b0db0c6a477897ef70a8f1308131075a150
leehjhjhj <tsukiakarii@naver.com>
2024-03-25 15:27:19 +0900

    add: 라운드 체킹 커스텀 에러 추가

--- src/sportslive/utils/exceptions/game_exceptions.py
+++ src/sportslive/utils/exceptions/game_exceptions.py
@@ -7,3 +7,9 @@ class CantDeleteGameError(APIException):
 class CantParsingYoutubeUrl(APIException):
     status_code = 400
     default_detail = "유튜브 링크 형식이 잘못되었습니다."
+    default_code = "cant_parsing_youtube_url"
+
+class BiggerThanMaxRoundError(APIException):
+    status_code = 400
+    default_detail = "리그의 최대 라운드 보다 크게 할 수 없습니다."
+    default_code = "bigger_than_max_round_error"
