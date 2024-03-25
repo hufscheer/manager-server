b875672dcead9359f8ede0bc4208f808db05ba66
leehjhjhj <tsukiakarii@naver.com>
2024-03-25 12:58:16 +0900

    feat: 라인업 제외 대회 팀 선수 불러오는 로직 추가

--- src/sportslive/team/application/team_player_get_service.py
+++ src/sportslive/team/application/team_player_get_service.py
@@ -1,6 +1,6 @@
 from team.domain import TeamPlayerRepository, TeamRepository, LeagueTeamPlayer
 from team.serializers import TeamPlayersResponseSerialier
-from game.domain import GameRepository, GameTeam
+from game.domain import GameRepository, GameTeam, LineupPlayer
 
 class TeamPlayerGetService:
     def __init__(
@@ -20,6 +20,9 @@ class TeamPlayerGetService:
     
     def get_all_team_player_by_game_team(self, game_team_id: int):
         game_team: GameTeam = self._game_repository.find_game_team_by_id(game_team_id)
-        team_players: list[LeagueTeamPlayer] = self._team_player_repository.find_team_player_by_team_id(game_team.league_team_id)
+        current_lineup_players: list[LineupPlayer] = self._game_repository.find_lineup_players_by_game_team_id(game_team.id)
+        current_lineup_players_ids = [current_lineup_player.league_team_player_id for current_lineup_player in current_lineup_players]
+        team_players: list[LeagueTeamPlayer] = self._team_player_repository.find_team_player_by_team_id_exclude_lineup_player(game_team.league_team_id, current_lineup_players_ids)
+
         team_player_data = TeamPlayersResponseSerialier(team_players, many=True)
         return team_player_data.data
