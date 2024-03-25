37d3c43e956e8e8b0a61661342be4b950fa5d1bd
leehjhjhj <tsukiakarii@naver.com>
2024-03-25 13:41:06 +0900

    refactor: 대회 팀 수정시 하나의 필드만 수정 가능하도록 로직 변경

--- src/sportslive/team/application/team_service.py
+++ src/sportslive/team/application/team_service.py
@@ -14,22 +14,27 @@ class TeamService:
 
     def change_team(self, request_data, team_id: int, user_data: Member):
         team: LeagueTeam = self._team_repository.find_team_with_league_by_id(team_id)
+        team_name = team.name
         if team.organization != user_data.organization:
             raise PermissionDenied
         team_change_request_serializer = TeamChangeRequestSerializer(data=request_data)
         team_change_request_serializer.is_valid(raise_exception=True)
         team_change_data = team_change_request_serializer.validated_data
     
-        team_name = team_change_data.get('name')[0]
-        team_logo = team_change_data.get('logo')[0]
+        team_name_list = team_change_data.get('name', None)
+        team_logo_list = team_change_data.get('logo', None)
+        if team_name_list:
+            team_name = team_name_list[0]
+            team.name = team_name
+        
+        if team_logo_list:
+            team_logo = team_logo_list[0]
+            s3_key = self._s3_conn.make_s3_key(image_data=team_logo, team_name=team_name, league_name=team.league.name)
+            self._sqs_conn.send_message_to_sqs(s3_key)
+            logo_url = self._s3_conn.upload_to_s3(image_data=team_logo, team_name=team_name, league_name=team.league.name)
+            team.logo_image_url = logo_url
 
-        s3_key = self._s3_conn.make_s3_key(image_data=team_logo, team_name=team_name, league_name=team.league.name)
-        logo_url = self._s3_conn.upload_to_s3(image_data=team_logo, team_name=team_name, league_name=team.league.name)
-
-        team.name = team_name
-        team.logo_image_url = logo_url
         self._team_repository.save_team(team)
-        self._sqs_conn.send_message_to_sqs(s3_key)
 
     def delete_team(self, team_id: int, user_data: Member):
         team: LeagueTeam = self._team_repository.find_team_with_league_by_id(team_id)
